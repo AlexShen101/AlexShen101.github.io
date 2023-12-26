8a38a23f2ee8ed66747cecb94035e372e4770943
alexander shen <alexander.shen@gmail.com>
2023-12-25 21:30:20 -0500

    changing urls for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const repo = 'v1-portfolio'
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   output: "export"
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  basePath: basePath,
+}
